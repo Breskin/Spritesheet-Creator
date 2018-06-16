0ba53ce38a2d4a5d83ba7f0ac4c690383fc43f18
Jakub Piasecki <jakubpiasecki67@outlook.com>
2018-06-16 18:31:10 +0200

    Add project files.

---
+++ SpriteSheetCreator/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SpriteSheetCreator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string[] Files = { };
+        public int Columns = 1, Rows = 1;
+        public double Scale = 1;
+        public bool FlipHorizontally = false, FlipVertically = false;
+
+        MyCanvas Canvas;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Canvas = new MyCanvas(this);
+            Grid.Children.Add(Canvas);
+
+            ScaleSlider.Value = 1;
+            ColumnsSlider.Value = 1;
+        }
+
+        private void OpenFolder_Click(object sender, RoutedEventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == System.Windows.Forms.DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    Files = Directory.GetFiles(fbd.SelectedPath).Where(s => s.EndsWith(".png")).OrderBy(s => int.Parse(s.Substring(s.LastIndexOf("\\") + 1, s.LastIndexOf(".") - s.LastIndexOf("\\") - 1))).ToArray();
+
+                    ColumnsSlider.Maximum = ColumnsSlider.Value = Files.Length;
+                    ColumnsSlider.Minimum = 1;
+                    Rows = 1;
+
+                    Canvas.LoadImages();
+                    Canvas.UpdateSize();
+
+                    ScaleText.Content = Math.Floor(Scale * 100) + "%" + ((double.IsNaN(Canvas.Width)) ? "" : (" (" + (int)Canvas.Width + "x" + (int)Canvas.Height + ")"));
+
+                    Canvas.InvalidateVisual();
+                }
+            }
+        }
+
+        private void Generate_Click(object sender, RoutedEventArgs e)
+        {
+            Canvas.Generate();
+        }
+
+        private void Columns_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Columns = (int)Math.Round(ColumnsSlider.Value);
+            Rows = (int)Math.Ceiling((Files.Length * 1d) / (Columns * 1d));
+
+            if (Canvas != null)
+            {
+                ColumnsText.Content = Columns + "x" + Rows;
+                Canvas.UpdateSize();
+
+                ScaleText.Content = Math.Floor(Scale * 100) + "%" + ((double.IsNaN(Canvas.Width)) ? "" : (" (" + (int)Canvas.Width + "x" + (int)Canvas.Height + ")"));
+            }
+        }
+
+        private void HorizontalFlip_Click(object sender, RoutedEventArgs e)
+        {
+            FlipHorizontally = (bool)HorizontalFlip.IsChecked;
+
+            Canvas.InvalidateVisual();
+        }
+
+        private void VerticalFlip_Click(object sender, RoutedEventArgs e)
+        {
+            FlipVertically = (bool)VerticalFlip.IsChecked;
+
+            Canvas.InvalidateVisual();
+        }
+
+        private void Scale_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Scale = ScaleSlider.Value;
+            Canvas.UpdateSize();
+
+            ScaleText.Content = Math.Floor(Scale * 100) + "%" + ((double.IsNaN(Canvas.Width)) ? "" : (" (" + (int)Canvas.Width + "x" + (int)Canvas.Height + ")"));
+
+            Canvas.InvalidateVisual();
+        }
+    }
+}
---
+++ SpriteSheetCreator/MyCanvas.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace SpriteSheetCreator
+{
+    class MyCanvas : Canvas
+    {
+        MainWindow Window;
+
+        BitmapSource[] Images = { };
+
+        public MyCanvas(MainWindow window)
+        {
+            Window = window;
+        }
+
+        protected override void OnRender(DrawingContext dc)
+        {
+            if (Images.Length > 0)
+            {
+                for (int i = 0; i < Images.Length; i++)
+                {
+                    dc.DrawImage(Transform(Images[i]), new System.Windows.Rect(Images[i].PixelWidth * (i % Window.Columns) * Window.Scale, Images[i].PixelHeight * (i / Window.Columns) * Window.Scale, Images[i].PixelWidth * Window.Scale, Images[i].PixelHeight * Window.Scale));
+                }
+            }
+        }
+
+        public void LoadImages()
+        {
+            Images = new BitmapImage[Window.Files.Length];
+
+            for (int i = 0; i < Window.Files.Length; i++)
+            {
+                Images[i] = new BitmapImage(new Uri(Window.Files[i]));
+            }
+        }
+
+        public void UpdateSize()
+        {
+            if (Images.Length > 0)
+            {
+                Width = Window.Columns * Images[0].PixelWidth * Window.Scale;
+                Height = Window.Rows * Images[0].PixelHeight * Window.Scale;
+            }
+        }
+
+        public void Generate()
+        {
+            System.Windows.Forms.SaveFileDialog dialog = new System.Windows.Forms.SaveFileDialog();
+            dialog.Filter = "|*.png";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                System.Drawing.Bitmap bitmap = new System.Drawing.Bitmap((int)Width, (int)Height);
+                System.Drawing.Graphics g = System.Drawing.Graphics.FromImage(bitmap);
+
+                g.FillRectangle(new System.Drawing.SolidBrush(System.Drawing.Color.FromArgb(255, 0, 0, 0)), new System.Drawing.RectangleF(0, 0, bitmap.Width, bitmap.Height));
+
+                for (int i = 0; i < Images.Length; i++)
+                {
+                    g.DrawImage(BitmapImage2Bitmap(Transform(Images[i])), (int)(Images[i].PixelWidth * (i % Window.Columns) * Window.Scale), (int)(Images[i].PixelHeight * (i / Window.Columns) * Window.Scale), (int)(Images[i].PixelWidth * Window.Scale), (int)(Images[i].PixelHeight * Window.Scale));
+                }
+
+                bitmap.MakeTransparent();
+
+                bitmap.Save(dialog.FileName);
+            }
+        }
+
+        TransformedBitmap Transform(BitmapSource b)
+        {
+            Transform tr = new ScaleTransform((Window.FlipHorizontally) ? -1 : 1, (Window.FlipVertically) ? -1 : 1);
+
+            TransformedBitmap transformedBmp = new TransformedBitmap();
+            transformedBmp.BeginInit();
+            transformedBmp.Source = b;
+            transformedBmp.Transform = tr;
+            transformedBmp.EndInit();
+
+            return transformedBmp;
+        }
+
+        private System.Drawing.Bitmap BitmapImage2Bitmap(BitmapSource bitmapImage)
+        {
+            using (MemoryStream outStream = new MemoryStream())
+            {
+                BitmapEncoder enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(bitmapImage));
+                enc.Save(outStream);
+                System.Drawing.Bitmap bitmap = new System.Drawing.Bitmap(outStream);
+
+                return new System.Drawing.Bitmap(bitmap);
+            }
+        }
+    }
+}
